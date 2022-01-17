313c087c23f50dea80bd4c7ecc7c82bfbddd1174
ludo <lubomir.duricek@pm.me>
2022-01-17 14:56:39 +0100

    init

---
+++ main.py
@@ -0,0 +1,60 @@
+# import the pygame library and initialise the game engine
+
+from ast import While
+from tkinter.messagebox import NO
+import pygame
+
+pygame.init()
+
+# define colors
+WHITE = (255,255,255)
+DARKBLUE = (36,90,190)
+LIGHTBLUE = (0,176,240)
+RED = (255,0,0)
+ORANGE = (255,100,0)
+YELLOW = (255,255,0)
+
+score = 0
+lives = 3
+
+# open a new window
+size = (800,600)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Breakout Game")
+
+# the look will carry on until the user eists the game (e.g. clicks the close button)
+carryOn = True
+
+# the clock will be used to control how fast the screen updates
+clock = pygame.time.Clock()
+
+# -------- Main Program Loop --------
+while carryOn:
+    # --- Main event loop
+    for event in pygame.event.get(): # user did something
+        if event.type == pygame.QUIT: # if user clicked close
+            carryOn = False # flag what we are done so we exit this loop
+
+    # --- game logic
+
+    
+    # --- drawing code goes here
+    # first, clear the screen to dark blue
+    screen.fill(DARKBLUE)
+    pygame.draw.line(screen, WHITE, [0,38],[800,38],2)
+
+    # display the score and the number of lives at the top of the screen
+    font = pygame.font.Font(None, 34)
+    text = font.render("Score: "+ str(score), 1, WHITE)
+    screen.blit(text, (20,10))
+    text = font.render("Lives: "+ str(lives), 1, WHITE)
+    screen.blit(text, (650,10))
+
+    # --- update the screen with what we've drawn
+    pygame.display.flip()
+
+    # --- limit to 60 frames per second
+    clock.tick(60)
+
+# once we have exited the main program loop we can stop the game engine
+pygame.quit()
---
+++ paddle.py
@@ -0,0 +1,26 @@
+# import pygame
+from operator import length_hint
+import pygame
+
+#define color
+BLACK = (0,0,0)
+
+# define class
+class Paddle(pygame.sprite.Sprite):
+    # this class represents a paddle. It derives from the "Sprite" class in Pygame
+
+    def __init__(self, color, width, height) -> None:
+        # call the parent class (Sprite) constructor
+        super().__init__()
+
+        # pass in the color of the paddle, its width and height
+        # set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+
+        # draw the paddle (a rectangle)
+        pygame.draw.rect(self.image, color, [0,0, width, height])
+
+        # fetch the rectangle object that has the dimension of the image
+        self.rect = self.image.get_rect()
